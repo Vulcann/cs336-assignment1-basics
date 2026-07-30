b5d3e8b146978e790f2aa92ab50c98f131f467c4
Weiyu Yi <inweiyu@gmail.com>
2026-07-30 10:31:29 +0000

    get_batch is done

---
+++ cs336_basics/data.py
@@ -0,0 +1,40 @@
+import torch
+import random
+import numpy.typing as npt
+
+
+def get_batch(
+    dataset: npt.NDArray, batch_size: int, context_length: int, device: str
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """
+    Given a dataset (a 1D numpy array of integers) and a desired batch size and
+    context length, sample language modeling input sequences and their corresponding
+    labels from the dataset.
+
+    Args:
+        dataset (np.array): 1D numpy array of integer token IDs in the dataset.
+        batch_size (int): Desired batch size to sample.
+        context_length (int): Desired context length of each sampled example.
+        device (str): PyTorch device string (e.g., 'cpu' or 'cuda:0') indicating the device
+            to place the sampled input sequences and labels on.
+
+    Returns:
+        Tuple of torch.LongTensors of shape (batch_size, context_length). The first tuple item
+        is the sampled input sequences, and the second tuple item is the corresponding
+        language modeling labels.
+    """
+
+    import numpy as np
+    x = dataset
+    m = context_length
+    starts = np.random.randint(0, len(x) - context_length, size=(batch_size,))
+    inputs  = np.stack([x[i     : i + m    ] for i in starts])   # m = context_length
+    targets = np.stack([x[i + 1 : i + m + 1] for i in starts])
+    return torch.tensor(inputs, device=device), torch.tensor(targets, device=device)
+    # data_in = torch.empty(batch_size, context_length, dtype=int, device=device)
+    # data_target = torch.empty(batch_size, context_length, dtype=int, device=device)
+    # for k in range(batch_size):
+    #     i = random.randint(0, len(dataset) - context_length - 1)
+    #     data_in[k, :] = torch.from_numpy(dataset[i : i + context_length])
+    #     data_target[k, :] = torch.from_numpy(dataset[i + 1 : i + 1 + context_length])
+    # return (data_in, data_target)
--- tests/adapters.py
+++ tests/adapters.py
@@ -521,7 +521,10 @@ def run_get_batch(
         is the sampled input sequences, and the second tuple item is the corresponding
         language modeling labels.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    from cs336_basics import data
+
+    return data.get_batch(dataset=dataset, batch_size=batch_size, context_length=context_length, device=device)
 
 
 def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, " ..."]:
